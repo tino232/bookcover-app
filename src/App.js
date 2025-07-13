15c373a5775e129ba65c963aa9504a8055d613e9
Tino <tino@On-the-Air-24.local>
2025-07-13 23:32:12 +0700

    23:35 change

--- src/App.js
+++ src/App.js
@@ -562,7 +562,6 @@ export default function App() {
             min-height: calc(90vh - 62px) !important;
             min-width: 0;
             padding: 0 20px;
-            min-height: 0 !important;
             height: auto !important;
           }
           .export-canvas { width: ${EXPORT_CANVAS_SIZE_MOBILE}px !important; height: ${EXPORT_CANVAS_SIZE_MOBILE}px !important; max-width: 99vw; max-height: 99vw;}
