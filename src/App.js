adf7941eedb9d412d5c8c8390e9087695ce9237f
Tino <tino@On-the-Air-24.local>
2025-07-13 23:18:08 +0700

    23:20 change

--- src/App.js
+++ src/App.js
@@ -220,7 +220,7 @@ export default function App() {
                 <svg width="19" height="19" fill="none" stroke="#2ea9b6" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24"><rect x="9" y="9" width="13" height="13" rx="2" /><path d="M5 15V5a2 2 0 0 1 2-2h10" /></svg>
               </span>
             }
-            <span class="">Paste clipboard</span>
+            <span class="btn-title">Paste clipboard</span>
           </button>
           <label htmlFor="upload-image" className="upload-btn">
             <span className="btn-icon">
